6f0663abed8c1b876ba4ab0668f6ea4cea3fd929
JaydenAb <jaabrams@quinnipiac.edu>
2024-02-14 11:37:56 -0500

    added program to add

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Calculator{
+
+    public static void main(String[] args){
+         Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the first whole number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second whole number: ");
+        int num2 = scanner.nextInt();
+
+
+        int result = num1 + num2;
+        System.out.println("The result of " + num1 + " + " + num2 + " is: " + result);
+
+        scanner.close();
+    }
+}
