d7ceb24d7ab27f94e23c9f869f0ea546ae176ec0
JaydenAb <jaabrams@quinnipiac.edu>
2024-02-14 11:51:15 -0500

    added multiplication and divison

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,10 @@
+// Jayden Abrams
+// Calculator repo
+// 2/14/2024
+
+
+
+
 import java.util.Scanner;
 
 public class Calculator{
@@ -13,24 +20,46 @@ public class Calculator{
         int num2 = scanner.nextInt();
 
 
-        System.out.print("Choose an operation (1 for addition, 2 for subtraction): ");
+        System.out.println("Choose an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.print("Enter the number corresponding to your choice: ");
         int operationChoice = scanner.nextInt();
 
-        // Perform the selected operation
-        int result;
-        if (operationChoice == 1) {
-            // Addition
-            result = num1 + num2;
-            System.out.println("You chose addition. The result is: " + result);
-        } else if (operationChoice == 2) {
-            // Subtraction
-            result = num1 - num2;
-            System.out.println("You chose subtraction. The result is: " + result);
-        } else {
-            System.out.println("Invalid operation choice. Please choose 1 for addition or 2 for subtraction.");
+        
+        double result;
+        switch (operationChoice) {
+            case 1:
+                // Addition
+                result = num1 + num2;
+                System.out.println("You chose addition. The result is: " + result);
+                break;
+            case 2:
+                // Subtraction
+                result = num1 - num2;
+                System.out.println("You chose subtraction. The result is: " + result);
+                break;
+            case 3:
+                // Multiplication
+                result = num1 * num2;
+                System.out.println("You chose multiplication. The result is: " + result);
+                break;
+            case 4:
+                // Division
+                if (num2 != 0) {
+                    result = (double) num1 / num2;
+                    System.out.println("You chose division. The result is: " + result);
+                } else {
+                    System.out.println("Cannot divide by zero. Please enter a non-zero second number.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operation choice. Please enter a number between 1 and 4.");
         }
 
-        // Close the Scanner
+        
         scanner.close();
     }
 }
